104ead64d29abc6df82c0a62f32d4940b5484dc6
CuerdOS <cuerdoslinux@proton.me>
2024-06-02 15:19:22 +0200

    Change of structure

---
+++ spins-translations.js
@@ -0,0 +1,51 @@
+var translations =
+	{
+		//ENGLISH STRINGS
+		'en':
+		{
+            'str-menu-dev2':'Community',
+            'str-subtitle-spins1': 'What are CuerdOS Spins?',
+            'str-content-spins-1': 'CuerdOS Spins are variations of the distro with alternative desktop environments to the original project and a selection of applications of their own, thus offering different ways to experience CuerdOS. These projects are developed by members of the community.',
+			'str-content-downloadlink-3': 'Edition with the KDE Plasma desktop environment, its software suite changes to accommodate the entire Qt environment.',
+			'str-content-downloadlink-4': 'With the Cinnamon desktop environment, an edition that aims to be easy to use and user-friendly for all types of users.',
+			'str-content-downloadlink-5': 'The green plane distribution together with a modified Gnome Shell, to bring it back to its classic and familiar distribution.',
+			'str-desc-autor': 'These editions were created and are mainly maintained by <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
+			'str-subtitle-upgradewarning': '*Before testing the system, we strongly recommend that you upgrade to the latest version, as it will have all the new features and bugs fixed.*',
+			'str-subtitle-logolicense': 'The <a class="element" href="https://github.com/CuerdOS/logo">CuerdOS logo</a> is published under license <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
+			'str-credits': 'Created By CuerdOS Dev Team. ',
+		},
+		//SPANISH STRINGS
+		'es':
+		{
+            'str-menu-dev2':'Comunidad',
+            'str-subtitle-spins1': '¿Qué son los CuerdOS Spins?',
+            'str-content-spins-1': 'Los CuerdOS Spins son variaciones de la distro con entornos de escritorio alternativos al proyecto original y una selección de aplicaciones propia, por lo tanto ofrecen formas diferentes de experimentar CuerdOS. Estos proyectos son desarrollados por miembros de la comunidad.',
+			'str-content-downloadlink-3': 'Edición con el entorno de escritorio KDE Plasma, su suit de software cambia para adaptarse a todo el entorno de Qt.',
+			'str-content-downloadlink-4': 'CuerdOS y el escritorio Cinnamon, una edición que busca ser sencilla de usar y amigable para todo tipo de usuarios.',
+			'str-content-downloadlink-5': 'La distribución del avión verde junto a un Gnome Shell modificado, para devolverlo a su distribución clásica y familiar.',
+			'str-desc-autor': 'Estas ediciones fueron creadas y son principalmente mantenidas por <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
+			'str-subtitle-upgradewarning': '*Antes de probar el sistema, recomendamos encarecidamente que actualice a la última versión, ya que tendrá todas las nuevas características y errores corregidos.*',
+			'str-subtitle-logolicense': 'El <a class="element" href="https://github.com/CuerdOS/logo">logo de CuerdOS</a> está publicado bajo licencia <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
+			'str-credits': 'Creado por CuerdOS Dev Team. ',
+		},
+        //PORTUGUESE STRINGS
+		'pt':
+		{
+            'str-menu-dev2':'Comunidade',
+            'str-subtitle-spins1': 'O que são os CuerdOS Spins?',
+            'str-content-spins-1': 'Os CuerdOS Spins são variações da distribuição com ambientes de área de trabalho alternativos ao projeto original e uma seleção de aplicativos próprios, oferecendo assim diferentes maneiras de experimentar o CuerdOS. Esses projetos são desenvolvidos por membros da comunidade.',
+			'str-content-downloadlink-3': 'Edição com o ambiente de trabalho KDE Plasma, o seu pacote de software muda para acomodar todo o ambiente Qt.',
+			'str-content-downloadlink-4': 'CuerdOS + Cinnamon, uma edição que visa ser fácil de usar e amigável para todos os tipos de usuários.',
+			'str-content-downloadlink-5': 'O layout do plano verde juntamente com um Gnome Shell modificado, para o trazer de volta ao seu layout clássico e familiar.',
+			'str-desc-autor': 'Estas edições foram criadas e são mantidas principalmente por <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
+			'str-subtitle-upgradewarning': '*Antes de testar o sistema, recomendamos fortemente que você atualize para a versão mais recente, pois ela terá todas as novas funcionalidades e bugs corrigidos.*',
+			'str-subtitle-logolicense': 'O <a class="element" href="https://github.com/CuerdOS/logo">logotipo do CuerdOS</a> está publicado sob licença <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
+			'str-credits': 'Criado pela Equipe de Desenvolvimento do CuerdOS. ',
+		},
+		// ??? STRINGS
+		'fr':
+		{
+			//You can add other translations by putting language code (e.g fr, pt or it)
+		}
+		
+	};
