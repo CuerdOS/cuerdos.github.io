97e0f2f63f9add139da4405d893a579b7c6b795e
CuerdOS <cuerdoslinux@proton.me>
2024-06-04 12:44:18 +0200

    add cuerdos mate

--- spins-translations.js
+++ spins-translations.js
@@ -9,9 +9,8 @@ var translations =
 			'str-content-downloadlink-3': 'Edition with the KDE Plasma desktop environment, its software suite changes to accommodate the entire Qt environment.',
 			'str-content-downloadlink-4': 'With the Cinnamon desktop environment, an edition that aims to be easy to use and user-friendly for all types of users.',
 			'str-content-downloadlink-5': 'The green plane distribution together with a modified Gnome Shell, to bring it back to its classic and familiar distribution.',
+			'str-content-downloadlink-6': 'Edition with the Mate desktop environment, with a customization inspired by the ElementaryOS Pantheon desktop.',
 			'str-desc-autor': 'These editions were created and are mainly maintained by <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
-			'str-subtitle-upgradewarning': '*Before testing the system, we strongly recommend that you upgrade to the latest version, as it will have all the new features and bugs fixed.*',
-			'str-subtitle-logolicense': 'The <a class="element" href="https://github.com/CuerdOS/logo">CuerdOS logo</a> is published under license <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
 			'str-credits': 'Created By CuerdOS Dev Team. ',
 		},
 		//SPANISH STRINGS
@@ -23,9 +22,8 @@ var translations =
 			'str-content-downloadlink-3': 'Edición con el entorno de escritorio KDE Plasma, su suit de software cambia para adaptarse a todo el entorno de Qt.',
 			'str-content-downloadlink-4': 'CuerdOS y el escritorio Cinnamon, una edición que busca ser sencilla de usar y amigable para todo tipo de usuarios.',
 			'str-content-downloadlink-5': 'La distribución del avión verde junto a un Gnome Shell modificado, para devolverlo a su distribución clásica y familiar.',
+			'str-content-downloadlink-6': 'Edición con el entorno de escritorio Mate, con una customización inspirada inspirada en el escritorio Pantheon de ElementaryOS.',
 			'str-desc-autor': 'Estas ediciones fueron creadas y son principalmente mantenidas por <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
-			'str-subtitle-upgradewarning': '*Antes de probar el sistema, recomendamos encarecidamente que actualice a la última versión, ya que tendrá todas las nuevas características y errores corregidos.*',
-			'str-subtitle-logolicense': 'El <a class="element" href="https://github.com/CuerdOS/logo">logo de CuerdOS</a> está publicado bajo licencia <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
 			'str-credits': 'Creado por CuerdOS Dev Team. ',
 		},
         //PORTUGUESE STRINGS
@@ -37,9 +35,8 @@ var translations =
 			'str-content-downloadlink-3': 'Edição com o ambiente de trabalho KDE Plasma, o seu pacote de software muda para acomodar todo o ambiente Qt.',
 			'str-content-downloadlink-4': 'CuerdOS + Cinnamon, uma edição que visa ser fácil de usar e amigável para todos os tipos de usuários.',
 			'str-content-downloadlink-5': 'O layout do plano verde juntamente com um Gnome Shell modificado, para o trazer de volta ao seu layout clássico e familiar.',
+			'str-content-downloadlink-6': 'Edição com o ambiente de desktop Mate, com uma personalização inspirada no desktop ElementaryOS Pantheon.',
 			'str-desc-autor': 'Estas edições foram criadas e são mantidas principalmente por <a class="element" href="https://github.com/gatoverde95">Gatoverde95</a>.',
-			'str-subtitle-upgradewarning': '*Antes de testar o sistema, recomendamos fortemente que você atualize para a versão mais recente, pois ela terá todas as novas funcionalidades e bugs corrigidos.*',
-			'str-subtitle-logolicense': 'O <a class="element" href="https://github.com/CuerdOS/logo">logotipo do CuerdOS</a> está publicado sob licença <a class="element" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA-4.0</a>',
 			'str-credits': 'Criado pela Equipe de Desenvolvimento do CuerdOS. ',
 		},
 		// ??? STRINGS
