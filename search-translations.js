944fd6a95be97707bf14465729bab68e1c1d7376
CuerdOS <cuerdoslinux@proton.me>
2024-06-20 18:18:17 +0200

    add search page

---
+++ search-translations.js
@@ -0,0 +1,51 @@
+var translations =
+	{
+		//ENGLISH STRINGS
+		'en':
+		{
+            'str-topmenu-1':'Home',
+            'str-topmenu-2':'About',
+            'str-topmenu-3':'Download',
+            'str-topmenu-4':'Spins',
+            'str-topmenu-5':'Changelog',
+            'str-topmenu-6':'Sourceforge',
+            'str-topmenu-7':'GitHub',
+            'str-topmenu-8':'Telegram',
+            'str-search-1':'<input type="text" name="q" placeholder="Search on the web..." autofocus>',
+            'str-footer-1':'© 2024 CuerdOS Dev Team. This site is under <a class="whitelement" href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU GPL v3</a>.'
+		},
+		//SPANISH STRINGS
+		'es':
+		{
+            'str-topmenu-1':'Inicio',
+            'str-topmenu-2':'Acerca de',
+            'str-topmenu-3':'Descargas',
+            'str-topmenu-4':'Spins',
+            'str-topmenu-5':'Changelog',
+            'str-topmenu-6':'Sourceforge',
+            'str-topmenu-7':'GitHub',
+            'str-topmenu-8':'Telegram',
+            'str-search-1':'<input type="text" name="q" placeholder="Buscar en la web..." autofocus>',
+            'str-footer-1':'© 2024 CuerdOS Dev Team. Este sitio está bajo <a class="whitelement" href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU GPL v3</a>.'
+		},
+        //PORTUGUESE STRINGS
+		'pt':
+		{
+            'str-topmenu-1':'Início',
+            'str-topmenu-2':'Sobre a',
+            'str-topmenu-3':'Baixar',
+            'str-topmenu-4':'Spins',
+            'str-topmenu-5':'Changelog',
+            'str-topmenu-6':'Sourceforge',
+            'str-topmenu-7':'GitHub',
+            'str-topmenu-8':'Telegram',
+            'str-search-1':'<input type="text" name="q" placeholder="Busca na Web..." autofocus>',
+            'str-footer-1':'© 2024 CuerdOS Dev Team. Este site está sob <a class="whitelement" href="https://www.gnu.org/licenses/gpl-3.0.html" target="_blank">GNU GPL v3</a>.'
+		},
+		// ??? STRINGS
+		'fr':
+		{
+			//You can add other translations by putting language code (e.g fr, pt or it)
+		}
+		
+	};
